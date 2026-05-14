348f76617578d412e13c4c1244c03e1976399f3d
rayhantmt <rayhantmt@gmail.com>
2026-05-15 02:50:20 +0600

    basic if else block

---
+++ score.py
@@ -0,0 +1,9 @@
+score=int(input('Enter Your Score'))
+if score >80:
+    print('Your Grade is A+')
+elif score>60:
+    print("Your Grade is B+")
+elif score>40:
+    print("You barely passed")
+else:
+    print("You failed successfully")   

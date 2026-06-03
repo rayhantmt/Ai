a4079d0b1ab9aba610e89ee0073d44d40888767e
Rayhan <Rayhan@Betopias-Mac-mini-4.local>
2026-06-04 01:28:37 +0600

    created class and object

---
+++ sales_analysis/hello.py
@@ -0,0 +1,13 @@
+class dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        return "Woof!"
+    
+sahhrir = dog("Sahhrir", 3)
+print(sahhrir.name)
+print(sahhrir.age)
+
+print(sahhrir.bark()) 
